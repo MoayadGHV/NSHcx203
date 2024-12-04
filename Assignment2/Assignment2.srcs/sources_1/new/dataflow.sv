5a82ea2cc4f1fda796612ffe073a95931a2b7a0f
MoayadGHV <Moayadtalalgh@hotmail.com>
2024-12-04 15:17:38 +0300

    Assignmet2

---
+++ Assignment2/Assignment2.srcs/sources_1/new/dataflow.sv
@@ -0,0 +1,26 @@
+`timescale 1ns / 1ps
+//////////////////////////////////////////////////////////////////////////////////
+// Company: 
+// Engineer: 
+// 
+// Create Date: 12/04/2024 02:46:38 PM
+// Design Name: 
+// Module Name: dataflow
+// Project Name: 
+// Target Devices: 
+// Tool Versions: 
+// Description: 
+// 
+// Dependencies: 
+// 
+// Revision:
+// Revision 0.01 - File Created
+// Additional Comments:
+// 
+//////////////////////////////////////////////////////////////////////////////////
+
+
+module dataflow(
+
+    );
+endmodule
